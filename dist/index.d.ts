69afc23babf5e40016586872b950c4883e316c0a
Martynas Šiška <33836575+Martyneris@users.noreply.github.com>
2023-05-05 13:18:50 +0300

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -77,7 +77,7 @@ export declare type IProps = {
     stylePinCodeTextButtonCircle?: StyleProp<TextStyle>;
     stylePinCodeTextSubtitle?: StyleProp<TextStyle>;
     stylePinCodeTextTitle?: StyleProp<TextStyle>;
-    stylePinCodeViewTitle?: StyleProp<TextStyle>;
+    stylePinCodeViewTitle?: StyleProp<ViewStyle>;
     subtitleChoose?: string;
     subtitleComponent?: any;
     subtitleConfirm?: string;
